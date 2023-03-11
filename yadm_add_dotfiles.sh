c3569270289625b44fea28a32b9680d9d1b4d823
LimHyungTae <shapelim@kaist.ac.kr>
2023-03-11 17:01:06 +0900

    Remove visible file

--- yadm_add_dotfiles.sh
+++
@@ -1,7 +0,0 @@
-yadm add .zshenv .zshrc .zshrc_local
-yadm add .oh-my-zsh/custom/aliases.zsh .oh-my-zsh/custom/functions.zsh -f 
-yadm add .config/nvim
-yadm add .config/terminator/config
-
-yadm commit -m "$*"
-yadm push
