c19e50b787a66566ae5bfb71208104869f358ef0
Hyungtae Lim <shapelim@kaist.ac.kr>
2023-03-12 16:37:26 +0900

    Update .yadm_add_dotfiles file

--- .yadm_add_dotfiles.sh
+++ .yadm_add_dotfiles.sh
@@ -1,6 +1,7 @@
 yadm add .zshenv .zshrc .zshrc_local
 yadm add .oh-my-zsh/custom/aliases.zsh .oh-my-zsh/custom/functions.zsh -f 
 yadm add .config/nvim
+yadm add .config/lvim/config.lua
 yadm add .config/terminator/config
 
 yadm commit -m "$*"
